b9fa0f168319847542e4fc88996371b6fd521bac
Vladislavs Žabotinskis <vladislavs.zabotinskis@fintelum.com>
2024-03-12 19:56:23 +0200

    Add project files.

---
+++ labyrinth-crawler-game.cpp
@@ -0,0 +1,12 @@
+﻿// labyrinth-crawler-game.cpp : Defines the entry point for the application.
+//
+
+#include "labyrinth-crawler-game.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
